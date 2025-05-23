e344f79c642a3376e9b28f5a6684bcb474d20568
Asbjørn Knudsen <eaaatkn@students.eaaa.dk>
2025-05-23 14:05:02 +0200

    matrix, vektor added

---
+++ matrix.py
@@ -0,0 +1,33 @@
+﻿# Reduktion, SR & Imperativ
+def matrix_sum_of(m):
+    total_sum = 0
+    i = 0
+    while i < len(m):
+        row = m[i]
+        j = 0
+        while j < len(row):
+            total_sum += row[j]
+            j += 1
+        i += 1
+    return total_sum
+
+matrix = [
+    [1, 2, 3], # row 0
+    [4, 5, 6], # row 1
+    [7, 8, 9]  # row 2
+]
+
+# Tests
+def test_matrix_sum_of():
+    matrix1 = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    assert matrix_sum_of(matrix1) == 45
+
+    matrix3 = [
+        [-1, -2],
+        [3, 4]
+    ]
+    assert matrix_sum_of(matrix3) == 4
---
+++ vektor.py
@@ -0,0 +1,45 @@
+﻿# Afbildning, IP & Imperativ
+def add_vat(v, p):
+    if len(v) == 0:
+        raise ValueError("List is empty")
+
+    i = 0
+    while i < len(v):
+        v[i] = v[i] * p
+        i += 1
+    return v
+
+# Filtrering, SR & Imperativ
+def filter_vektor_between_2numbers(v, min_val, max_val):
+    result = []
+    i = 0
+    while i < len(v):
+        x = v[i]
+        if min_val <= x <= max_val:
+            result.append(x)
+        i += 1
+    return result
+
+
+
+# Tests
+def test_of_add_vat():
+    # arrange
+    input_v = [100, 200, 300]
+    input_p = 1.25
+    expected = [125.0, 250.0, 375.0]
+    # act
+    result = add_vat(input_v, input_p)
+    # assert
+    assert expected == result
+
+def test_of_add_vat_null():
+    try:
+        add_vat([], 1.25)
+        assert False
+    except ValueError:
+        assert True
+
+def test_filter_vektor_between_2numbers():
+    assert filter_vektor_between_2numbers([1, 4, 7, 10, 12], 3, 8) == [4, 7]
+
