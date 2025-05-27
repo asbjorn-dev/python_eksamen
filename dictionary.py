ab926c4fb798499d991fd279b5c39195e024cf5f
Augustmunkhaus <Fordemkidz69@gmail.com>
2025-05-27 13:07:33 +0200

    dictionary with tests added

--- dictionary.py
+++ dictionary.py
@@ -17,7 +17,27 @@ def sales_volume_per_agent(df: pl.DataFrame) -> pl.DataFrame:
     )
     return result
 
-# Test funktionen
 output = sales_volume_per_agent(df1)
 print(output)
+
+#Test
+def test_sales_volume_per_agent():
+    # Arrange
+    df_input = pl.DataFrame([
+        ['dk-34922423', 'pja', 55_960_00],
+        ['dk-34927049', 'pja', 35_905_31],
+        ['dk-34929950', 'ssk', 1_000_000_00],
+        ['dk-34929950', 'pja', 72_048_67]
+    ], schema=["order_id", "agent_id", "sales_volume"])
+
+    expected = pl.DataFrame([
+        ('pja', 55_960_00 + 35_905_31 + 72_048_67),
+        ('ssk', 1_000_000_00)
+    ], schema=["agent_id", "total_sales_volume"]).sort("agent_id")
+
+    # Act
+    result = sales_volume_per_agent(df_input).sort("agent_id")
+
+    # Assert
+    assert result.frame_equal(expected)
 
