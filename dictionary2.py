c06ffa61b34c75907fc1bf9b207c87d0b592a2d0
asbjorn-dev <eaaatkn@students.eaaa.dk>
2025-06-01 17:40:22 +0200

    addde dictionary2

---
+++ dictionary2.py
@@ -0,0 +1,25 @@
+﻿#dictionary løsning
+
+data = [
+    ['dk-34922423', 'pja', 55_960_00],
+    ['dk-34927049', 'pja', 35_905_31],
+    ['dk-34929950', 'ssk', 1_000_000_00],
+    ['dk-34929950', 'pja', 72_048_67]
+]
+
+def sales_volume_dictionary(d):
+    dictionary = {}
+    for row in d:
+        agent = row[1]
+        sales = row[2]
+
+        if agent in dictionary:
+            dictionary[agent]['total_sales'] += sales
+            dictionary[agent]['num_sales'] += 1
+        else:
+            dictionary[agent] = {'total_sales': sales, 'num_sales': 1}
+
+    return dictionary
+
+output = sales_volume_dictionary(data)
+print(output)
